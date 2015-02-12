e7aed8393729d566fc93b20ae5a93e3fabd5cf58
GrilloPress <grillopress@gmail.com>
2015-02-12 17:04:45 +0000

    add static pages controller

--- tern/config/routes.rb
+++ tern/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  root 'static_pages#index'
+  
+  StaticPagesController.action_methods.each do |action|
+    get "/#{action}", to: "static_pages##{action}", as: "#{action}"
+  end
+  
   resources "contacts", only: [:new, :create]
 
   devise_for :admins, ActiveAdmin::Devise.config
