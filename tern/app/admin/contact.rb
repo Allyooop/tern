ab497de5051661dfd4eaa0c6f9bf5f551f527ee6
GrilloPress <grillopress@gmail.com>
2015-02-12 11:50:43 +0000

    register Contact model with active admin

---
+++ tern/app/admin/contact.rb
@@ -0,0 +1,11 @@
+ActiveAdmin.register Contact do
+
+  # as with Rails controllers, white list actions
+  permit_params :name, :email, :message
+  
+  # enable the right-hand sidebar to search and filter on certain fields
+  filter :name
+  filter :email
+  filter :message
+  
+end
