6a02927b8d5e577875fac5fa9a54737e3b0ddcc4
GrilloPress <grillopress@gmail.com>
2015-02-12 00:11:58 +0000

    add contact form controller

--- tern/app/models/contact.rb
+++ tern/app/models/contact.rb
@@ -2,4 +2,19 @@ class Contact < MailForm::Base
   # name:string 
   # email:string 
   # message:text 
+  
+  validates :name, :email, :message, presence: true
+  
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+  
+  def headers
+    {
+      :subject => "My Contact Form",
+      :to => "grillopress@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+  
 end
